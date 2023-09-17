c121cce3a9cdca3af3d9005e1bf3aa12b6bac668
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2023-09-16 19:34:53 -0500

    Add templates

--- ScholarshipManagement/ScholarshipModule/views.py
+++ ScholarshipManagement/ScholarshipModule/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 # Create your views here.
+
+
+def signUp(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            # Register user :)
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists'
+                })
+
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'The passwords do not match'
+        })
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        
+        if user == None:
+            return render(request, 'login.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
+        
+        
