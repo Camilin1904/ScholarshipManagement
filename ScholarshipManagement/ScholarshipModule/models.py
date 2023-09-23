1e9958a667ae785e857e0b9375046535671d8908
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2023-09-22 21:52:01 -0500

    Add non functional implementation of foreign key

--- ScholarshipManagement/ScholarshipModule/models.py
+++ ScholarshipManagement/ScholarshipModule/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Donors(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    ID = models.IntegerField(
+        primary_key=True, auto_created=True, serialize=True, unique=True)
 
 class Scholarships(models.Model):
     name = models.CharField(max_length=100, blank=False)
     ID = models.IntegerField(
         primary_key=True, auto_created=True, serialize=True, unique=True)
     description = models.TextField(blank=True)
-    donor = models.CharField(max_length=100, blank=False)
+    donor = models.ForeignKey(Donors, on_delete= models.CASCADE)
     coverage = models.FloatField(blank=False)
     type = models.IntegerField(blank=False)
     requirements = models.TextField(blank=True)
 
-class Donors(models.Model):
-    name = models.CharField(max_length=100, blank=False)
-    ID = models.IntegerField(
-        primary_key=True, auto_created=True, serialize=True, unique=True)
