998d4716ddbff05d8781f6cf970e7bad3c298066
AndresCamiloRR <1105361345@u.icesi.edu.co>
2023-09-28 16:06:42 -0500

    Reformat improving CSS done

--- ScholarshipManagement/ScholarshipModule/views.py
+++ ScholarshipManagement/ScholarshipModule/views.py
@@ -74,8 +74,15 @@ def signIn(request):
 # The tag will demand the user to login
 @login_required(login_url="/login")
 def home(request):
-
-    return render(request, './HTML/home.html')
+    user = request.user
+    if user.role == 0:
+        return render(request, './HTML/homeAdmin.html')
+    elif user.role == 1:
+        return render(request, './HTML/homeFinancial.html')
+    elif user.role == 2:
+        return render(request, './HTML/homePhilanthropy.html')
+    else:
+        return render(request, './HTML/notAcces.html')
 
 
 def scholarships(request):
