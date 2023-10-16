f2cd6573a2b11f30ccb2412fdad018222d9c303d
BrayanOrteg <brayansteven0322@gmail.com>
2023-10-15 21:39:33 -0500

    Add AnnouncementApplicant test

---
+++ ScholarshipManagement/ScholarshipModule/tests/testAppliAnnounModel.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from ScholarshipModule.models import AnnouncementAndApplicant
+from ScholarshipModule.models import Applicant
+from ScholarshipModule.models import Announcements
+
+
+
+class TestApplicant(TestCase):
+
+    def testCreationApplicant(self):
+        announcementTest = Announcements.objects.create(id = 1, type = 0)
+        applicantTest = Applicant.objects.create(
+            name = "Juan Carlos", lastName = "Rios Garcia", ID = 10,
+            studentCode = "A00302613", faculty = "Ingenieria", major = "Sistemas",
+            semester = 2, email = "juanCar22@gmail.com", phone = 322717233)
+        relation = AnnouncementAndApplicant.objects.create(
+            id = 1, announcement= announcementTest , applicantID = applicantTest)
+        self.assertEqual(relation.id, 1)
+        self.assertEqual(relation.announcement.id, 1)
+        self.assertEqual(relation.applicantID.ID, 10)
+
+
