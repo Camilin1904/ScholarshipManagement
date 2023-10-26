487ffbb0ad4bf8f027ef83299ea74d93c066bc99
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2023-10-26 14:36:13 -0500

    Fix report generatio, Add untested logit to generate the report

--- ScholarshipManagement/ScholarshipModule/views2/pdf.py
+++ ScholarshipManagement/ScholarshipModule/views2/pdf.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.template.loader import get_template
 from xhtml2pdf import pisa
 from django.contrib.staticfiles import finders
+from datetime import date
+from ScholarshipModule.models import AnnouncementEvent
 
 
 #must install:
@@ -23,9 +25,7 @@ def link_callback(uri, rel):
    media_url = settings.MEDIA_URL         # Typically /media/
    media_root = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
    base_dir = settings.BASE_DIR           # Project's base directory
-   print("kkkkkkk", uri)
-   print(static_root)
-   print(static_url)
+
    if uri.startswith(media_url):
       path = os.path.join(media_root, uri.replace(media_url, ""))
    elif uri.startswith(static_url):
@@ -33,10 +33,6 @@ def link_callback(uri, rel):
       path = os.path.join(static_root, uri.replace(static_url, ""))
    else:
       return os.path.join(base_dir, '../', uri)
-   
-   print("aaaaa", path)
-   
-   print( os.path.isfile(path))
 
    # make sure that file exists
    if not os.path.isfile(path):
@@ -46,11 +42,17 @@ def link_callback(uri, rel):
    return path
 
 def render_pdf_view(request):
-     
+   #Get the require announcement
+   try:
+      announcementNum = request.sessions.get('announcementNum')
+      announcement = AnnouncementEvent.objects.filter(announcementId_id = announcementNum)
+      context = {'date':date.today(), 'announcement': announcement}
+   except:
+      context = {'date':date.today(), 'error':'La convocatoria pedida no existe.'}
+      
+      
    template_path = 'HTML/reportTemplate.html'
    static_url = settings.STATIC_URL 
-   print("ofjhsgdiu", static_url)
-   context = {}
    # Create a Django response object, and specify content_type as pdf
    response = HttpResponse(content_type='application/pdf')
    #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
