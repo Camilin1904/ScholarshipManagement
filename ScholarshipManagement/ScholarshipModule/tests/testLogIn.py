2c06b8f9238a2dfe31ce0049b4b2aa9cf84e330a
SebasL07 <100957510+SebasL07@users.noreply.github.com>
2023-11-08 14:37:04 -0500

    Add log in and sign in tests

--- ScholarshipManagement/ScholarshipModule/tests/testLogIn.py
+++ ScholarshipManagement/ScholarshipModule/tests/testLogIn.py
@@ -3,13 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+class testLogInAndSignIn(LiveServerTestCase):
 
-class SignInTest(LiveServerTestCase):
-    
-    def testSignIn(self):
-        driver = webdriver.Chrome()
+    def test1SignIn(self):
+        driver = webdriver.Edge()
         
-        driver.get('http://127.0.0.1:8000/')
+        driver.get('http://127.0.0.1:3000/')
 
         username = driver.find_element(by=By.ID,value='id_username')
         username.click()
@@ -37,8 +36,31 @@ class SignInTest(LiveServerTestCase):
 
         assert 'Home' in driver.title
 
+    def test2LogIn(self):
 
+        driver = webdriver.Edge()
 
+        driver.get('http://127.0.0.1:3000/')
 
+        login = driver.find_element(by=By.ID,value='login')
+        login.click()
+
+        driver.implicitly_wait(10)
+
+        username = driver.find_element(by=By.ID,value='id_username')
+        username.click()
+        username.clear()
+        username.send_keys('correoprueba@gmail.com')
+
+        password = driver.find_element(by=By.ID,value='id_password')
+        password.click()
+        password.clear()
+        password.send_keys('prueba9170')
+
+        submit = driver.find_element(by=By.ID,value='signin')
+        submit.send_keys(Keys.RETURN)
+
+        assert 'Home' in driver.title
 
     
+
